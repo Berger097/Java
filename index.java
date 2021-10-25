130372414a92e76483b0cc0d5c25b9f1f71333ce
Anatoly Moskalev <moskalev1997tolya@gmail.com>
2021-10-25 16:46:16 +0600

    commitTwo

---
+++ index.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int currentBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int totalSum = currentBalance + transferAmount;
+        System.out.println(totalSum);
+    }
+}
